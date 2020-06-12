f845c41e38a29699f6162db7e9b9a0bd40d2456f
Mark Tse <mark.tse@neverendingqs.com>
2020-06-12 15:42:17 -0400

    app - updating index and some metadata.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Tools by neverendingqs', // process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: 'Miscellaneous tools by neverendingqs',
+        name: 'Miscellaneous tools by neverendingqs',
+        content: 'Miscellaneous tools by neverendingqs' // process.env.npm_package_description || ''
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        web-tools
-      </h1>
-      <h2 class="subtitle">
-        A collection of tools.
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="app">
+    <p class="display-4">Pick a tool to start!</p>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue';
-
-export default {
-  components: {
-    Logo
-  }
-};
-</script>
+<style scoped>
+#app {
+  text-align: center;
+}
+</style>
