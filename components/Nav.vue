0c8f5cab1dfabae7aa8b7801db516384a2379021
Mark Tse <mark.tse@neverendingqs.com>
2020-06-12 16:03:30 -0400

    app - adding links to other tools.

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,6 +3,24 @@
     <nuxt-link class="navbar-brand" to="/">Tools</nuxt-link>
     <ul class="navbar-nav d-flex justify-content-end">
       <li>
+        <a
+          class="nav-link"
+          href="https://oauth2-client-shell.neverendingqs.com/"
+        >
+          OAuth 2.0 Client Shell
+        </a>
+      </li>
+      <li>
+        <a class="nav-link" href="https://pprint-ndjson.neverendingqs.com/">
+          Pretty-Print Newline-Delimited JSON
+        </a>
+      </li>
+      <li>
+        <a class="nav-link" href="https://string-utils.neverendingqs.com/">
+          String Utils
+        </a>
+      </li>
+      <li>
         <nuxt-link class="nav-link" to="/wsg">Waste Some Guids</nuxt-link>
       </li>
     </ul>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <p class="display-4">Pick a tool to start!</p>
+    <p>
+      Links may you to other sites. Please be patient as tools are being
+      consolidated.
+    </p>
   </div>
 </template>
 
