59d18959d85488697c3b1c7a9240e68a9fc94fcb
Mark Tse <mark.tse@neverendingqs.com>
2020-06-13 15:35:20 -0400

    app - added ExternalLink component.

---
+++ components/ExternalLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :class="classes" :href="href" target="_blank" rel="noopener noreferrer">
+    <slot></slot><sup>🔗</sup>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ExternalLink',
+  props: {
+    classes: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
--- components/Nav.vue
+++ components/Nav.vue
@@ -3,17 +3,20 @@
     <nuxt-link class="navbar-brand" to="/">Tools</nuxt-link>
     <ul class="navbar-nav d-flex justify-content-end ml-auto">
       <li>
-        <a
-          class="nav-link"
+        <external-link
+          classes="nav-link"
           href="https://oauth2-client-shell.neverendingqs.com/"
         >
           OAuth 2.0 Client Shell
-        </a>
+        </external-link>
       </li>
       <li>
-        <a class="nav-link" href="https://pprint-ndjson.neverendingqs.com/">
+        <external-link
+          classes="nav-link"
+          href="https://pprint-ndjson.neverendingqs.com/"
+        >
           Pretty-Print Newline-Delimited JSON
-        </a>
+        </external-link>
       </li>
       <li>
         <nuxt-link class="nav-link" to="/stringutils">
@@ -26,3 +29,13 @@
     </ul>
   </nav>
 </template>
+
+<script>
+import ExternalLink from '~/components/ExternalLink';
+
+export default {
+  components: {
+    ExternalLink
+  }
+};
+</script>
