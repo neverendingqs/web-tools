25ce871996c2143ff76f2c57a103f8ba765cd084
Mark Tse <mark.tse@d2l.com>
2021-08-03 09:55:29 -0400

    fix: revert bootstrap major bump and fix lint.

--- components/wsg/Guid.vue
+++ components/wsg/Guid.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="form-inline justify-content-center">
+    <!-- eslint-disable-next-line vue/no-mutating-props -->
     <input v-model="guid" type="text" readonly="" class="form-control" />
     <button v-clipboard:copy="guid" class="btn btn-primary">Copy</button>
   </div>
